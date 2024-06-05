effa9b8ec75f4ebd90047a8286618b6334e7cf0a
Radoslaw Palka <radekpalka@gmail..com>
2024-06-05 19:22:02 +0200

    create Header.tsx, add dependencies to package.json

--- I am sober/src/App.tsx
+++ I am sober/src/App.tsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-
-
+import { useState } from 'react';
+import { Header } from './tsx/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
-  
+	return (
+		<>
+			<Header />
 
-  return (
-    <>
-      <h1>I am sober</h1>
-    </>
-  )
-}
+			<Router>
+				<Routes>
+					<Route path='/' />
+					<Route path='/dashboard' />
+				</Routes>
+			</Router>
+		</>
+	);
+};
 
-export default App
+export default App;
---
+++ I am sober/src/tsx/Header.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+export const Header : React.FC = () =>{
+  return(
+    <header>
+      <p className="gratulations">Przede wszystkim gratulacje</p>
+      <p className="message">Właśnie zrobiłeś duży krok ku trzeźwości</p>
+    </header>
+  )
+}
