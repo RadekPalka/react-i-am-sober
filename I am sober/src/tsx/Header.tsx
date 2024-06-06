3aa3f0b990de75ea132fee2925318c83e659df10
Radoslaw Palka <radekpalka@gmail..com>
2024-06-06 19:16:37 +0200

    Added styled-components for Header and paragraphs

--- I am sober/src/tsx/Header.tsx
+++ I am sober/src/tsx/Header.tsx
@@ -1,9 +1,21 @@
 import styled from 'styled-components'
+const StyledHeader = styled.header`
+  
+  width: 50%;
+  margin: 100px auto 0 auto;
+`
+const StyledCongratulationsParagraph = styled.p`
+  text-align: center;
+  font-weight: 600;
+`
+const StyledParagraph = styled.p`
+  text-align: center;
+`
 export const Header : React.FC = () =>{
   return(
-    <header>
-      <p className="gratulations">Przede wszystkim gratulacje</p>
-      <p className="message">Właśnie zrobiłeś duży krok ku trzeźwości</p>
-    </header>
+    <StyledHeader>
+      <StyledCongratulationsParagraph>Przede wszystkim gratulacje</StyledCongratulationsParagraph>
+      <StyledParagraph>Właśnie zrobiłeś duży krok ku trzeźwości</StyledParagraph>
+    </StyledHeader>
   )
 }
