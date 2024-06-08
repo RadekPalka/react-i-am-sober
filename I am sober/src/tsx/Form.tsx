6480efe401f126e9a71ea3640b71d278260be33b
Radoslaw Palka <radekpalka@gmail..com>
2024-06-08 15:33:25 +0200

    delete data-value from option tag in form

--- I am sober/src/tsx/Form.tsx
+++ I am sober/src/tsx/Form.tsx
@@ -13,10 +13,10 @@ export const Form : React.FC<FormProps> = ({onSubmit}) =>{
       <label htmlFor="addiction-choice'">Od czego jesteś uzależniony</label>
       <input id='addiction-choice' list='Addiction-type'/>
       <datalist id='Addiction-type'>
-        <option value="Alkohol" data-value="alcohol"/>
-        <option value="Narkotyki" data-value="drugs"/>
-        <option value="Pornografia" data-value="pornography"/>
-        <option value="Hazard" data-value="gamble"/>
+        <option value="Alkohol"/>
+        <option value="Narkotyki"/>
+        <option value="Pornografia"/>
+        <option value="Hazard"/>
       </datalist>
       <button type='submit'>Dalej</button>
     </form>
