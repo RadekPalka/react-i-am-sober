66d03d02083b252cd7bebdaa78d221e9b28f8164
Radoslaw Palka <radekpalka@gmail..com>
2024-06-13 19:14:15 +0200

    create StyledDiv and StyledButton

--- I am sober/src/tsx/Form.tsx
+++ I am sober/src/tsx/Form.tsx
@@ -4,39 +4,16 @@ import { AddictionInput } from './AddictionInput';
 import { DateInput } from './DateInput';
 import { DailyCostInput } from './DailyCostInput';
 
-
-
-
 const StyledForm = styled.form`
 	display: flex;
 	flex-direction: column;
 	justify-content: space-around;
 	width: 75%;
 	margin: 0 auto;
-	.input-container {
-		display: flex;
-		justify-content: space-between;
-		flex-wrap: wrap;
-		margin-bottom: 10px;
-		padding: 30px 0;
-	}
-	button {
-		display: block;
-		width: 100px;
-		height: 50px;
-		color: #fff;
-		margin: 0 auto;
-		background-color: #693eb7;
-		text-align: center;
-		font-family: 'LXGW WenKai TC', cursive;
-		transition: background-color 0.5s;
-		cursor: pointer;
-		&:hover {
-			background-color: #8b60d9;
-		}
-	}
 `;
 
+const StyledDiv = styled.div``;
+const StyledButton = styled.button``;
 export const Form: React.FC = () => {
 	const navigate = useNavigate();
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -45,16 +22,16 @@ export const Form: React.FC = () => {
 	};
 	return (
 		<StyledForm onSubmit={handleSubmit}>
-			<div className='input-container'>
+			<StyledDiv>
 				<AddictionInput />
-			</div>
-			<div className='input-container'>
+			</StyledDiv>
+			<StyledDiv>
 				<DateInput />
-			</div>
-			<div className='input-container'>
-				<DailyCostInput/>
-			</div>
-			<button type='submit'>Dalej</button>
+			</StyledDiv>
+			<StyledDiv>
+				<DailyCostInput />
+			</StyledDiv>
+			<StyledButton type='submit'>Dalej</StyledButton>
 		</StyledForm>
 	);
 };
