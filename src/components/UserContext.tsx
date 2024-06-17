e6bfa80f17cbf92b605b4edeab036bdc80cc49eb
Radoslaw Palka <radekpalka@gmail..com>
2024-06-17 20:36:17 +0200

    delete condition from useUserContext in UserContext.tsx

--- src/components/UserContext.tsx
+++ src/components/UserContext.tsx
@@ -6,9 +6,7 @@ const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export const useUserContext = () => {
 	const context = useContext(UserContext);
-	if (!context) {
-		throw new Error('useUserContext must be used within a UserProvider');
-	}
+
 	return context;
 };
 
