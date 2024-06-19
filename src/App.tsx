cb726178e3e73db77c7b3ed926d9b61efbfdd6e0
Radoslaw Palka <radekpalka@gmail..com>
2024-06-19 18:44:04 +0200

    style: :art: change style in AppWrapper

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 import { Route, Routes } from 'react-router-dom';
 import { AddictionInfoForm } from './pages/AddictionInfoForm';
-import { UserProvider } from './components/UserContext'; 
-import { Dashboard, MainPage, RegistrationForm, LoginForm } from './pages'
+import { UserProvider } from './components/UserContext';
+import { Dashboard, MainPage, RegistrationForm, LoginForm } from './pages';
 
 const AppWrapper = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 20px;
+	display: flex;
+	max-width: 1200px;
+	height: 100vh;
+	justify-content: center;
 `;
 
 const App: React.FC = () => {
-  return (
-    <AppWrapper>
-      <UserProvider>
-        <Routes>
-          <Route path='/' element={<MainPage />}/>
-          <Route path="/addiction-info" element={<AddictionInfoForm />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/registration-page" element={<RegistrationForm />}/>
-          <Route path="/login-page" element={<LoginForm />}/>
-        </Routes>
-      </UserProvider>
-    </AppWrapper>
-  );
+	return (
+		<AppWrapper>
+			<UserProvider>
+				<Routes>
+					<Route path='/' element={<MainPage />} />
+					<Route path='/addiction-info' element={<AddictionInfoForm />} />
+					<Route path='/dashboard' element={<Dashboard />} />
+					<Route path='/registration-page' element={<RegistrationForm />} />
+					<Route path='/login-page' element={<LoginForm />} />
+				</Routes>
+			</UserProvider>
+		</AppWrapper>
+	);
 };
 
 export default App;
