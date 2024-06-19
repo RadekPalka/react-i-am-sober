1e35d98251962d763df57fc78486547c2539a1dc
Radoslaw Palka <radekpalka@gmail..com>
2024-06-19 19:13:24 +0200

    refactor: add StyledNav to MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,15 +2,19 @@ import { Link } from 'react-router-dom';
 import { Header } from '../components/';
 import styled from 'styled-components';
 
+const StyledNav = styled.nav``;
+
 const StyledLink = styled(Link)`
-  
-`
+	position: absolute;
+`;
 export const MainPage: React.FC = () => {
 	return (
 		<>
 			<Header />
-			<StyledLink to='/registration-page'>Zarejestruj się</StyledLink>
-			<StyledLink to='/login-page'>Zaloguj się</StyledLink>
+			<StyledNav>
+				<StyledLink to='/registration-page'>Zarejestruj się</StyledLink>
+				<StyledLink to='/login-page'>Zaloguj się</StyledLink>
+			</StyledNav>
 		</>
 	);
 };
