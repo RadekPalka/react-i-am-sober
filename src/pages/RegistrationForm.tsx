ae1b5f1725ec5384cc1a844af52c2ffbc1f518b1
Radoslaw Palka <radekpalka@gmail..com>
2024-06-20 18:27:18 +0200

    feat: add functions

--- src/pages/RegistrationForm.tsx
+++ src/pages/RegistrationForm.tsx
@@ -3,8 +3,15 @@ import { AuthInput } from '../components';
 export const RegistrationForm: React.FC = () => {
 	const [login, setLogin] = useState('');
 	const [password, setPassword] = useState('');
-  const loginLabelText = 'Podaj swój login'
-  const passwordLabelText = 'Podaj swoje hasło'
+	const loginLabelText = 'Podaj swój login';
+	const passwordLabelText = 'Podaj swoje hasło';
+	const handleLoginChange = (value: string) => {
+		setLogin(value);
+	};
+
+	const handlePasswordChange = (value: string) => {
+		setPassword(value);
+	};
 	return (
 		<>
 			<h1>Rejestracja</h1>
