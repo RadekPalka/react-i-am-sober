11efa075ebb79be8d58e8f8894d229233978e1a7
Radoslaw Palka <radekpalka@gmail..com>
2024-06-20 18:20:12 +0200

    feat: add labels text

--- src/pages/RegistrationForm.tsx
+++ src/pages/RegistrationForm.tsx
@@ -3,6 +3,8 @@ import { AuthInput } from '../components';
 export const RegistrationForm: React.FC = () => {
 	const [login, setLogin] = useState('');
 	const [password, setPassword] = useState('');
+  const loginLabelText = 'Podaj swój login'
+  const passwordLabelText = 'Podaj swoje hasło'
 	return (
 		<>
 			<h1>Rejestracja</h1>
