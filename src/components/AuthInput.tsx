1a141a66de22affe84d87671f11663dfb095d5b2
Radoslaw Palka <radekpalka@gmail..com>
2024-06-20 18:42:49 +0200

    feat: update AuthInput to accept label and value props

--- src/components/AuthInput.tsx
+++ src/components/AuthInput.tsx
@@ -2,12 +2,23 @@ import { ChangeEvent } from 'react';
 
 import { StyledDiv } from './StyledDiv';
 import { StyledInput } from './StyledInput';
-
-export const AuthInput: React.FC = () => {
+type AuthInputProps = {
+	labelText: string;
+	value: string;
+	onChange: (value: string) => void;
+};
+export const AuthInput: React.FC<AuthInputProps> = ({
+	labelText,
+	value,
+	onChange,
+}) => {
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		onChange(e.target.value);
+	};
 	return (
 		<StyledDiv>
-			<label htmlFor='login'>Podaj swój login</label>
-			<StyledInput type='text' id='login' />
+			<label htmlFor='login'>{labelText}</label>
+			<StyledInput value={value} onChange={handleChange} />
 		</StyledDiv>
 	);
 };
