240bf48ec72cc7de6a843d7bb7059da77226fc8c
Radoslaw Palka <radekpalka@gmail..com>
2024-06-20 18:01:31 +0200

    fix: delete useConext hook from AuthInput component

--- src/components/AuthInput.tsx
+++ src/components/AuthInput.tsx
@@ -1,25 +1,13 @@
 import { ChangeEvent } from 'react';
-import { useUserContext } from './UserContext';
+
 import { StyledDiv } from './StyledDiv';
 import { StyledInput } from './StyledInput';
-import { UserData } from '../types/UserData';
+
 export const AuthInput: React.FC = () => {
-	const { userData, setUserData } = useUserContext();
-	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-		setUserData((prevState: UserData) => ({
-			...prevState,
-			login: e.target.value,
-		}));
-	};
 	return (
 		<StyledDiv>
 			<label htmlFor='login'>Podaj swój login</label>
-			<StyledInput
-				value={userData.login}
-				onChange={handleChange}
-				type='text'
-				id='login'
-			/>
+			<StyledInput type='text' id='login' />
 		</StyledDiv>
 	);
 };
