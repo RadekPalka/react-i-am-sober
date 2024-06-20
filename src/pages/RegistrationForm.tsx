375bbb48ed72aa1623e773ebd309627c76023b2d
Radoslaw Palka <radekpalka@gmail..com>
2024-06-20 18:16:33 +0200

    feat: add useState and form

--- src/pages/RegistrationForm.tsx
+++ src/pages/RegistrationForm.tsx
@@ -1,10 +1,15 @@
-import { LoginInputRegistration, PasswordInputRegistration } from "../components"
-export const RegistrationForm : React.FC = () =>{
-  return(
-    <>
-      <h1>Rejestracja</h1>
-      <LoginInputRegistration/>
-      <PasswordInputRegistration/>
-    </>
-  )
-}
+import { useState } from 'react';
+import { AuthInput } from '../components';
+export const RegistrationForm: React.FC = () => {
+	const [login, setLogin] = useState('');
+	const [password, setPassword] = useState('');
+	return (
+		<>
+			<h1>Rejestracja</h1>
+			<form>
+				<AuthInput />
+				<AuthInput />
+			</form>
+		</>
+	);
+};
