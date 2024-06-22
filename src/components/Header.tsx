b44525a80647e0e8cc4e443d22baaa325da49c47
Radoslaw Palka <radekpalka@gmail..com>
2024-06-22 18:24:23 +0200

    refactor: move nav element inside header for better semantic structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 const StyledHeader = styled.header`
 	width: 50%;
 	margin: 0 auto;
+`;
+const StyledNav = styled.nav``;
+
+const StyledLink = styled(Link)`
+	position: absolute;
 `;
 const StyledH1 = styled.h1`
 	text-align: center;
@@ -16,6 +22,10 @@ const StyledParagraph = styled.p`
 export const Header: React.FC = () => {
 	return (
 		<StyledHeader>
+			<StyledNav>
+				<StyledLink to='/registration-page'>Zarejestruj się</StyledLink>
+				<StyledLink to='/login-page'>Zaloguj się</StyledLink>
+			</StyledNav>
 			<StyledH1>
 				I am sober - aplikacja pomagająca w wyjściu z uzależnienia. Monitoruj
 				swoją trzeźwość, śledź postępy i utrzymuj zdrowy styl życia.
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,20 +1,9 @@
-import { Link } from 'react-router-dom';
 import { Header } from '../components/';
-import styled from 'styled-components';
 
-const StyledNav = styled.nav``;
-
-const StyledLink = styled(Link)`
-	position: absolute;
-`;
 export const MainPage: React.FC = () => {
 	return (
 		<>
 			<Header />
-			<StyledNav>
-				<StyledLink to='/registration-page'>Zarejestruj się</StyledLink>
-				<StyledLink to='/login-page'>Zaloguj się</StyledLink>
-			</StyledNav>
 		</>
 	);
 };
