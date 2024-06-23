cf176df4ee31f734650b3c0f1a3349b378e82bbd
Radoslaw Palka <radekpalka@gmail..com>
2024-06-23 16:42:16 +0200

    refactor: add return type in validation function

--- src/pages/RegistrationForm.tsx
+++ src/pages/RegistrationForm.tsx
@@ -4,37 +4,43 @@ import { useNavigate } from 'react-router-dom';
 export const RegistrationForm: React.FC = () => {
 	const [login, setLogin] = useState('');
 	const [password, setPassword] = useState('');
-	const [confirmPassword, setConfirmPassword] = useState('')
-	const navigate= useNavigate()
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const navigate = useNavigate();
 	const loginLabelText = 'Podaj swój login';
 	const passwordLabelText = 'Podaj swoje hasło';
-	const confirmPasswordLabelText = 'Potwierdź hasło'
-	
-	const handleChange = (callback :React.Dispatch<React.SetStateAction<string>>, value : string) =>{
-		callback(value)
-	}
-	const validateLogin = () : boolean =>{
+	const confirmPasswordLabelText = 'Potwierdź hasło';
+
+	const handleChange = (
+		callback: React.Dispatch<React.SetStateAction<string>>,
+		value: string
+	) => {
+		callback(value);
+	};
+	const validateLogin = (): boolean => {
 		const minLoginLength = 3;
 		return login.length > minLoginLength;
-	}
+	};
 	const validatePassword = (): boolean => {
 		const passwordRegex =
 			/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 		return passwordRegex.test(password);
 	};
-	const validateConfirmPassword = (password: string, confirmPassword: string) => {
-    return password === confirmPassword;
-  };
+	const validateConfirmPassword = (
+		password: string,
+		confirmPassword: string
+	): boolean => {
+		return password === confirmPassword;
+	};
 	const handleForm = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		if (!validateLogin){
-			return alert("Login musi mieć co najmniej 4 znaki")
-		}
-		else if (!validatePassword) {
+		if (!validateLogin) {
+			return alert('Login musi mieć co najmniej 4 znaki');
+		} else if (!validatePassword) {
 			return alert('Hasło musi zawierać znak specjalny, literą i cyfrę');
-		}
-		else if (validateConfirmPassword(password, confirmPassword)){
-			return alert('Hasła nie są zgodne. Proszę upewnić się, że oba hasła są identyczne.')
+		} else if (validateConfirmPassword(password, confirmPassword)) {
+			return alert(
+				'Hasła nie są zgodne. Proszę upewnić się, że oba hasła są identyczne.'
+			);
 		}
 		navigate('./login-page');
 	};
