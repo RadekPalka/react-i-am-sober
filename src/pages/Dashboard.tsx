4b44d48c957ce6ec4fefe151c7a365dd8faf790e
Radoslaw Palka <radekpalka@gmail..com>
2024-06-30 20:21:58 +0200

    feat: send request with session token

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,31 @@
-import { Form } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Form } from '../components/Form';
 import { useUserContext } from '../components/UserContext';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export const Dashboard: React.FC = () => {
 	const { userData } = useUserContext();
+	const navigate = useNavigate();
+	useEffect(() => {
+		const token = localStorage.getItem('sessionToken');
+		if (token) {
+			axios
+				.get('https://mentoring-api.vercel.app/api/v1/account/me', {
+					headers: {
+						Authorization: `${token}`,
+					},
+				})
+				.then((response) => {
+					console.log(response.data);
+				})
+				.catch((error) => {
+					console.error('Error fetching data:', error);
+				});
+		} else {
+			navigate('/');
+		}
+	}, []);
 	console.log(userData);
 	return (
 		<div>
