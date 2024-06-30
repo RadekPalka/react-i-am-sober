ef3c2b42bfd49b60a71f2e0b4520c714cc985cfd
Radoslaw Palka <radekpalka@gmail..com>
2024-06-30 21:08:08 +0200

    fix: move logic from dashboard to addictionInfoForm

--- src/pages/AddictionInfoForm.tsx
+++ src/pages/AddictionInfoForm.tsx
@@ -1,9 +1,51 @@
 import { Form } from '../components';
+import { useEffect } from 'react';
 
+import { useUserContext } from '../components/UserContext';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { UserData } from '../types/UserData';
+
+import { StyledSection } from '../components/StyledSection';
+import { StyledH1 } from '../components/StyledH1';
+import { HeadingContainer } from '../components/HeadingContainer';
 export const AddictionInfoForm: React.FC = () => {
+	const { userData, setUserData } = useUserContext();
+	const navigate = useNavigate();
+	useEffect(() => {
+		const token = localStorage.getItem('sessionToken');
+		if (token) {
+			axios
+				.get('https://mentoring-api.vercel.app/api/v1/account/me', {
+					headers: {
+						Authorization: `${token}`,
+					},
+				})
+				.then((response) => {
+					console.log(response.data);
+					setUserData((prevState: UserData) => ({
+						...prevState,
+						id: response.data.id,
+						login: response.data.username,
+					}));
+				})
+				.catch((error) => {
+					console.error('Error fetching data:', error);
+				});
+		} else {
+			navigate('/');
+		}
+	}, []);
+	console.log(userData);
 	return (
 		<>
-			<Form />
+			<StyledSection>
+				<HeadingContainer>
+					<StyledH1>Witaj {userData.login}</StyledH1>
+				</HeadingContainer>
+
+				<Form />
+			</StyledSection>
 		</>
 	);
 };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,49 +1,12 @@
-import { useEffect } from 'react';
-
-import { useUserContext } from '../components/UserContext';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { UserData } from '../types/UserData';
-import { AddictionInfoForm } from './AddictionInfoForm';
-import { StyledSection } from '../components/StyledSection';
-import { StyledH1 } from '../components/StyledH1';
 import { HeadingContainer } from '../components/HeadingContainer';
+import { StyledH1 } from '../components/StyledH1';
 
 export const Dashboard: React.FC = () => {
-	const { userData, setUserData } = useUserContext();
-	const navigate = useNavigate();
-	useEffect(() => {
-		const token = localStorage.getItem('sessionToken');
-		if (token) {
-			axios
-				.get('https://mentoring-api.vercel.app/api/v1/account/me', {
-					headers: {
-						Authorization: `${token}`,
-					},
-				})
-				.then((response) => {
-					console.log(response.data);
-					setUserData((prevState: UserData) => ({
-						...prevState,
-						id: response.data.id,
-						login: response.data.username,
-					}));
-				})
-				.catch((error) => {
-					console.error('Error fetching data:', error);
-				});
-		} else {
-			navigate('/');
-		}
-	}, []);
-	console.log(userData);
 	return (
-		<StyledSection>
+		<>
 			<HeadingContainer>
 				<StyledH1>Dashboard</StyledH1>
 			</HeadingContainer>
-			<p>Witaj {userData.login}</p>
-			<AddictionInfoForm />
-		</StyledSection>
+		</>
 	);
 };
