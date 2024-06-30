ed1d2fd6f42924c6d6dbf9d635551aca11c80526
Radoslaw Palka <radekpalka@gmail..com>
2024-06-30 17:13:25 +0200

    feat: set login and password states

--- src/pages/LoginForm.tsx
+++ src/pages/LoginForm.tsx
@@ -3,15 +3,23 @@ import { StyledInput } from '../components/StyledInput';
 import { StyledH1 } from '../components/StyledH1';
 import { StyledSection } from '../components/StyledSection';
 import { AuthInput } from '../components';
+import { useState } from 'react';
 
 export const LoginForm: React.FC = () => {
+	const [login, setLogin] = useState('');
+	const [password, setPassword] = useState('');
 	return (
 		<StyledSection>
 			<StyledH1>Strona logowania</StyledH1>
 			<StyledForm>
-				<AuthInput value='' labelText='Login' onChange={() => {}} type='text' />
 				<AuthInput
-					value=''
+					value={login}
+					labelText='Login'
+					onChange={() => {}}
+					type='text'
+				/>
+				<AuthInput
+					value={password}
 					labelText='Hasło'
 					onChange={() => {}}
 					type='password'
