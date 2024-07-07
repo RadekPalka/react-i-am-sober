fd6a49819f10f9b1da52bd8c6355270c67e5eeb5
RadekPalka <radekpalka@gmail.com>
2024-07-07 17:12:30 +0200

    refactor: move StyledLink to separate file

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+
 import { StyledUl } from './StyledUl';
 import { StyledLi } from './StyledLi';
 
@@ -10,13 +10,7 @@ const StyledNav = styled.nav`
 	height: 100px;
 `;
 
-const StyledLink = styled(Link)`
-	margin-right: 20px;
 
-	&:visited {
-		color: black;
-	}
-`;
 
 
 export const Nav: React.FC = () => {
---
+++ src/components/StyledLink.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { Link } from 'react-router-dom';
+
+export const StyledLink = styled(Link)`
+	margin-right: 20px;
+
+	&:visited {
+		color: black;
+	}
+`;
