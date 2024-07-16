80fd3a20b38552fdc70994b18dd9f7082548125f
Radoslaw Palka <radekpalka@gmail..com>
2024-07-16 18:28:42 +0200

    fix(test): correct and update tests for RegistrationForm component

--- jest.config.js
+++ jest.config.js
@@ -3,16 +3,9 @@ export default {
 	transform: {
 		'^.+\\.tsx?$': 'ts-jest',
 	},
+
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 	collectCoverage: true,
-	coverageReporters: ['text', 'lcov', 'html'],
-	coverageThreshold: {
-		global: {
-			branches: 80,
-			functions: 80,
-			lines: 80,
-			statements: 80,
-		},
-	},
+	coverageReporters: ['text-summary', 'lcov'],
 };
--- src/__tests__/pages/RegistrationForm.test.tsx
+++ src/__tests__/pages/RegistrationForm.test.tsx
@@ -1,16 +1,44 @@
 import { render, screen } from '@testing-library/react';
 import { RegistrationForm } from '../../pages';
 import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
 import React from 'react';
-describe('Check number of inputs', () => {
-	it('should have three inputs', () => {
+
+describe('RegistrationForm Component', () => {
+	it('should render the login input with correct label', async () => {
 		render(
 			<BrowserRouter>
 				<RegistrationForm />
 			</BrowserRouter>
 		);
 
-		const inputElements = screen.findAllByRole('textbox');
-		expect(inputElements).toHaveLength(3);
+		const loginInputElement = await screen.findByLabelText('Podaj swój login');
+		expect(loginInputElement).toBeInTheDocument();
+	});
+
+	it('should render the password input with correct label', async () => {
+		render(
+			<BrowserRouter>
+				<RegistrationForm />
+			</BrowserRouter>
+		);
+
+		const passwordInputElement = await screen.findByLabelText(
+			'Podaj swoje hasło'
+		);
+		expect(passwordInputElement).toBeInTheDocument();
+	});
+
+	it('should render the confirm password input with correct label', async () => {
+		render(
+			<BrowserRouter>
+				<RegistrationForm />
+			</BrowserRouter>
+		);
+
+		const confirmPasswordInputElement = await screen.findByLabelText(
+			'Potwierdź hasło'
+		);
+		expect(confirmPasswordInputElement).toBeInTheDocument();
 	});
 });
