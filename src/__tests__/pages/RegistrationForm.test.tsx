47071ef6a7d4d01ad04779980ca5d1c4a45a0ef0
Radoslaw Palka <radekpalka@gmail..com>
2024-07-23 19:14:17 +0200

    refactor: refacor tests

--- src/__tests__/pages/RegistrationForm.test.tsx
+++ src/__tests__/pages/RegistrationForm.test.tsx
@@ -14,55 +14,25 @@ jest.mock('react-toastify', () => ({
 }));
 
 describe('RegistrationForm Component', () => {
+	let loginInput: HTMLElement;
+	let passwordInput: HTMLElement;
+	let confirmPasswordInput: HTMLElement;
+	let submitButton: HTMLElement;
 	beforeEach(() => {
 		jest.clearAllMocks();
-	});
-	it('should render the login input with correct label', async () => {
 		render(
 			<BrowserRouter>
 				<RegistrationForm />
 			</BrowserRouter>
 		);
 
-		const loginInputElement = await screen.findByLabelText('Podaj swój login');
-		expect(loginInputElement).toBeInTheDocument();
+		loginInput = screen.getByLabelText('Podaj swój login');
+		passwordInput = screen.getByLabelText('Podaj swoje hasło');
+		confirmPasswordInput = screen.getByLabelText('Potwierdź hasło');
+		submitButton = screen.getByRole('button', { name: /zarejestruj się/i });
 	});
 
-	it('should render the password input with correct label', async () => {
-		render(
-			<BrowserRouter>
-				<RegistrationForm />
-			</BrowserRouter>
-		);
-
-		const passwordInputElement = await screen.findByLabelText(
-			'Podaj swoje hasło'
-		);
-		expect(passwordInputElement).toBeInTheDocument();
-	});
-
-	it('should render the confirm password input with correct label', async () => {
-		render(
-			<BrowserRouter>
-				<RegistrationForm />
-			</BrowserRouter>
-		);
-
-		const confirmPasswordInputElement = await screen.findByLabelText(
-			'Potwierdź hasło'
-		);
-		expect(confirmPasswordInputElement).toBeInTheDocument();
-	});
 	it('should show error message if login is too short', async () => {
-		render(
-			<BrowserRouter>
-				<RegistrationForm />
-			</BrowserRouter>
-		);
-
-		const loginInput = screen.getByLabelText('Podaj swój login');
-		const submitButton = screen.getByRole('button');
-
 		await userEvent.type(loginInput, 'abc');
 		await userEvent.click(submitButton);
 
@@ -71,23 +41,24 @@ describe('RegistrationForm Component', () => {
 		);
 	});
 
-	it('should show error message if login is incorrect', async () => {
-		render(
-			<BrowserRouter>
-				<RegistrationForm />
-			</BrowserRouter>
-		);
-
-		const loginInput = screen.getByLabelText('Podaj swój login');
-		const passwordInput = screen.getByLabelText('Podaj swoje hasło');
-		const submitButton = screen.getByRole('button');
-
+	it('should show error message if password is incorrect', async () => {
 		await userEvent.type(loginInput, 'login');
 		await userEvent.type(passwordInput, '!secretpassword');
 		await userEvent.click(submitButton);
 
 		expect(toast.error).toHaveBeenCalledWith(
 			'Hasło musi zawierać znak specjalny, literą i cyfrę'
+		);
+	});
+
+	it('should show error message if password and confirm password dosent match', async () => {
+		await userEvent.type(loginInput, 'login');
+		await userEvent.type(passwordInput, '!1secretpassword');
+		await userEvent.type(confirmPasswordInput, '!secretpassword');
+		await userEvent.click(submitButton);
+
+		expect(toast.error).toHaveBeenCalledWith(
+			'Hasła nie są zgodne. Proszę upewnić się, że oba hasła są identyczne.'
 		);
 	});
 });
