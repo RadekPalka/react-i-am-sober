c030c00f3f3ba3ae026b8286837a8ec116bc39d4
Radoslaw Palka <radekpalka@gmail..com>
2024-07-28 18:22:00 +0200

    test: add tests for LoginFormm component

---
+++ src/__tests__/pages/LoginForm.test.tsx
@@ -0,0 +1,68 @@
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import { LoginForm } from '../../pages';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import React from 'react';
+import { toast } from 'react-toastify';
+import { loginAction } from '../../clients/AccountClients';
+
+jest.mock('../../clients/AccountClients', () => ({
+	loginAction: jest.fn(),
+}));
+jest.mock('react-toastify', () => ({
+	toast: {
+		error: jest.fn(),
+		success: jest.fn(),
+	},
+}));
+
+describe('LoginForm component', () => {
+	let loginInput: HTMLElement;
+	let passwordInput: HTMLElement;
+	let submitButton: HTMLElement;
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+		render(
+			<BrowserRouter>
+				<LoginForm />
+			</BrowserRouter>
+		);
+
+		loginInput = screen.getByLabelText('Login');
+		passwordInput = screen.getByLabelText('Hasło');
+
+		submitButton = screen.getByRole('button', { name: /zaloguj się/i });
+	});
+	it('should show error message if login is too short', async () => {
+		await userEvent.type(loginInput, 'abc');
+		await userEvent.click(submitButton);
+
+		expect(toast.error).toHaveBeenCalledWith(
+			'Login musi mieć co najmniej 4 znaki'
+		);
+	});
+
+	it('should show error message if password is incorrect', async () => {
+		await userEvent.type(loginInput, 'login');
+		await userEvent.type(passwordInput, '!secretpassword');
+		await userEvent.click(submitButton);
+
+		expect(toast.error).toHaveBeenCalledWith(
+			'Hasło musi zawierać znak specjalny, literą i cyfrę'
+		);
+	});
+	it('should call loginAction with correct arguments when form is submitted with valid data', async () => {
+		await userEvent.type(loginInput, 'radekp');
+		await userEvent.type(passwordInput, '!1superhaslo');
+
+		await userEvent.click(submitButton);
+
+		expect(loginAction).toHaveBeenCalledWith(
+			'radekp',
+			'!1superhaslo',
+			expect.any(Function)
+		);
+	});
+});
