ca00c089fc3a7d1be3436e7ed422dfdd6c047ef9
Radoslaw Palka <radekpalka@gmail..com>
2024-08-05 18:30:35 +0200

    testing

--- src/__tests__/clients/AccountClient.test.ts
+++ src/__tests__/clients/AccountClient.test.ts
@@ -17,7 +17,6 @@ jest.mock('react-toastify', () => ({
 const mockedApi = api as jest.Mocked<typeof api>;
 
 describe('api tests', () => {
-<<<<<<< HEAD
 	beforeEach(() => {
 		jest.clearAllMocks();
 		console.log = jest.fn();
@@ -26,18 +25,11 @@ describe('api tests', () => {
 		mockedApi.post.mockResolvedValueOnce({ data: {} });
 		await createAccount('login', 'password', mockNavigate);
 		expect(console.log).toHaveBeenCalled();
-=======
-	it('should show success message and navigate on successful account creation', async () => {
-		mockedApi.post.mockResolvedValueOnce({ data: {} });
-		await createAccount('login', 'password', mockNavigate);
-
->>>>>>> b64042cbd7c6da1e464ae62e8da30a1d21b2e64f
 		expect(toast.success).toHaveBeenCalledWith(
 			'Rejestracja zakończona sukcesem'
 		);
 		expect(mockNavigate).toHaveBeenCalledWith('/login-page');
 	});
-<<<<<<< HEAD
 	it('should show error when username is already taken', async () => {
 		mockedApi.post.mockRejectedValueOnce({ data: {} });
 
@@ -45,6 +37,4 @@ describe('api tests', () => {
 		expect(console.log).toHaveBeenCalledWith('Error');
 		//expect(toast.error).toHaveBeenCalledWith('Podany login jest już zajęty');
 	});
-=======
->>>>>>> b64042cbd7c6da1e464ae62e8da30a1d21b2e64f
 });
