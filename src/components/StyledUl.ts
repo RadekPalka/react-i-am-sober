8e6f0321de4348a64d15eb19ce3935500b8c1761
Radosław Pałka <radekpalka@gmail.com>
2024-08-08 18:56:54 +0200

    style: center li elements vertically

--- src/components/StyledUl.ts
+++ src/components/StyledUl.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 export const StyledUl = styled.ul`
+	display: flex;
+	align-items: center;
 	list-style-type: none;
 	margin-right: 5px;
 	width: fit-content;
