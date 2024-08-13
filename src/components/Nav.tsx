ca67c3c7aafd22958fa9c6667a189c5287d5c93a
Radoslaw Palka <radekpalka@gmail..com>
2024-08-13 17:37:59 +0200

    refactor: remove unnecessary lines

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,8 @@
 import { StyledNav } from './StyledNav';
-import { StyledLink } from './StyledLink';
-import { StyledUl } from './StyledUl';
-import { StyledLi } from './StyledLi';
+
 import React from 'react';
 import { NavProps } from '../types/NavProps';
 
-export const Nav: React.FC<NavProps> = ({children}) => {
-	return (
-		<StyledNav>
-			{children}
-		</StyledNav>
-	);
+export const Nav: React.FC<NavProps> = ({ children }) => {
+	return <StyledNav>{children}</StyledNav>;
 };
