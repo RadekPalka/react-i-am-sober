73c3860b8a97d366d812cdd813a3b275640b60ec
Radoslaw Palka <radekpalka@gmail..com>
2024-08-20 17:50:42 +0200

    style: delete border and background-color

--- src/components/StyledNav.ts
+++ src/components/StyledNav.ts
@@ -5,9 +5,6 @@ type StyledNavProps = {
 export const StyledNav = styled.nav<StyledNavProps>`
 	display: flex;
 	justify-content: ${(props) => props.$justifyContent};
-	background-color: white;
-	border: solid 2px black;
 	right: 0;
-
 	height: 100px;
 `;
