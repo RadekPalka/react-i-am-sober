90ee22310896684691c4188813f1d23c975be02e
Radoslaw Palka <radekpalka@gmail..com>
2024-08-21 20:16:39 +0200

    feat: add nav to dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,10 @@
 import { HeadingContainer } from '../components/HeadingContainer';
 import { StyledH1 } from '../components/StyledH1';
 import React from 'react';
+import { StyledNav } from '../components/StyledNav';
+import { StyledUl } from '../components/StyledUl';
+import { StyledLi } from '../components/StyledLi';
+import { StyledLink } from '../components/StyledLink';
 export const Dashboard: React.FC = () => {
 	return (
 		<>
@@ -10,6 +14,15 @@ export const Dashboard: React.FC = () => {
 					monitorowania. Aby rozpocząć, kliknij poniższy link i wypełnij krótki
 					formularz, który pomoże Ci śledzić swoje postępy.
 				</StyledH1>
+				<StyledNav $justifyContent='center'>
+					<StyledUl>
+						<StyledLi $color='#e3e3e3' $background='#2c2c2c'>
+							<StyledLink to='/addiction-info'>
+								Dodaj nowe uzależnienie
+							</StyledLink>
+						</StyledLi>
+					</StyledUl>
+				</StyledNav>
 			</HeadingContainer>
 		</>
 	);
