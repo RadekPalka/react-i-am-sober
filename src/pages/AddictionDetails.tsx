f18ce5492352f15f1be53edb61eff871e69427c0
Radoslaw Palka <radekpalka@gmail..com>
2024-09-13 20:48:06 +0200

    feat: add state with  addiction details object from api

--- src/pages/AddictionDetails.tsx
+++ src/pages/AddictionDetails.tsx
@@ -1,24 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getAddictionDetails } from '../clients/AccountClients';
-
+type AddictionDetailsProps = {
+	id: number;
+	name: string;
+	costPerDay: number;
+	createdAt: string;
+	deadline: string;
+	lastIncidents: { id: number; createdAt: string }[];
+	numberOfIncidents: number;
+	limitOfLastIncidents: number;
+};
 export const AddictionDetails: React.FC = () => {
 	const { addictionId } = useParams();
+	const [addictionDetails, setAddictionDetails] =
+		useState<AddictionDetailsProps>({
+			id: 0,
+			name: '',
+			costPerDay: 0,
+			createdAt: '',
+			deadline: '',
+			lastIncidents: [],
+			numberOfIncidents: 0,
+			limitOfLastIncidents: 0,
+		});
 
 	useEffect(() => {
 		console.log(addictionId);
 		if (addictionId) {
-			const id = +addictionId?.slice(1, addictionId.length - 1);
+			const id = +addictionId?.slice(1, addictionId.length);
 
 			getAddictionDetails(id)
-				.then((res) => console.log(res))
+				.then((res) => {
+					setAddictionDetails(res.data);
+				})
 				.catch((error) => console.log(error));
 		}
 	}, []);
 
 	return (
 		<>
-			<h1>{addictionId}</h1>
+			<h1>{addictionDetails.name}</h1>
 		</>
 	);
 };
