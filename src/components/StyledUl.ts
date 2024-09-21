7ed714dfcbd15c0f909ba5cc910b539f05b77570
Radoslaw Palka <radekpalka@gmail..com>
2024-09-21 17:51:52 +0200

    feat: add optional props to justify-content

--- src/components/StyledUl.ts
+++ src/components/StyledUl.ts
@@ -1,11 +1,11 @@
-import styled from "styled-components";
-type UlStyledProps ={
-
-}
+import styled from 'styled-components';
+type UlStyledProps = {
+	$justifyContent?: string;
+};
 export const StyledUl = styled.ul<UlStyledProps>`
 	display: flex;
 	align-items: center;
-	justify-content: space-between;
+	justify-content: ${(props) => props.$justifyContent || 'space-between'};
 	list-style-type: none;
 	margin-right: 5px;
 	width: 200px;
