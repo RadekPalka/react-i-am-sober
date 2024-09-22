5e0713103a76543d073727b6aa28812461e8a97f
Radoslaw Palka <radekpalka@gmail..com>
2024-09-22 17:28:52 +0200

    style: add bigger margin

--- src/components/StyledUl.ts
+++ src/components/StyledUl.ts
@@ -8,6 +8,6 @@ export const StyledUl = styled.ul<UlStyledProps>`
 	align-items: center;
 	justify-content: ${(props) => props.$justifyContent || 'space-between'};
 	list-style-type: none;
-	margin-right: 5px;
+	margin-right: 20px;
 	width: ${(props) => props.$width || '200px'};
 `;
