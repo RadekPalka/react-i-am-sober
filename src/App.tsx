c07e01c8c152b37668da10be1f1976ed2799d342
Radoslaw Palka <radekpalka@gmail..com>
2024-11-12 18:29:28 +0100

    style: change font-family

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const AppWrapper = styled.div`
 	flex-direction: column;
 
 	height: max-content;
-	font-family: 'LXGW WenKai TC', cursive;
+	font-family: 'LXGW WenKai TC', sans-serif;
 	font-weight: 300;
 	font-style: normal;
 `;
