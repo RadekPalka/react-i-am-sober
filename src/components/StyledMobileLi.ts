dab9bf0f471f754151253af0b0f06c321e915d6c
Radoslaw Palka <radekpalka@gmail..com>
2024-11-17 17:19:13 +0100

    refactor: remove unnecessary components

--- src/components/StyledMobileLi.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledMobileLi = styled.li`
-	display: flex;
-	justify-content: center;
-	border-bottom: 1px solid white;
-	width: 100%;
-`;
--- src/components/StyledMobileUl.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledMobileUl = styled.ul`
-	display: flex;
-	justify-content: center;
-	flex-direction: column;
-	width: 100%;
-	list-style-type: none;
-`;
