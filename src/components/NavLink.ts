4b022f99b903e3fcc17b0df5d3cd8bcffce094f4
RadekPalka <radekpalka@gmail.com>
2024-11-20 04:42:46 +0100

    style: add style to NavLink

--- src/components/NavLink.ts
+++ src/components/NavLink.ts
@@ -3,8 +3,14 @@ import { Link } from 'react-router-dom';
 
 export const NavLink = styled(Link)`
 	display: block;
-	width: 90px;
+	width: 150px;
 	height: 30px;
+	background-color: black;
+	color: white;
+	text-decoration: none;
+	border-radius: 15px;
+	text-align: center;
+	align-content: center;
 
 	@media (max-width: 768px) {
 		width: 100%;
