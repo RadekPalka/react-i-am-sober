e63af6b52969f8278215ca6a5e77f5d4f5ac8591
Radoslaw Palka <radekpalka@gmail..com>
2024-11-20 17:54:25 +0100

    refactor: remove unnesacery lines

--- src/components/NavBarWrapper.ts
+++ src/components/NavBarWrapper.ts
@@ -4,38 +4,6 @@ type Props = {
 	$justifyContent?: 'start' | 'end' | 'space-between' | 'space-around';
 };
 export const NavBarWrapper = styled.nav<Props>`
-	top: 50px;
+	display: flex;
 	justify-content: ${(props) => props.$justifyContent || 'space-around'};
-	right: 0;
-
-	@media (min-width: 769px) {
-		display: flex;
-		position: static;
-		background-color: transparent;
-		width: 100%;
-	}
-	@media (max-width: 768px) {
-		width: 100%;
-		background-color: black;
-		color: white;
-		border-radius: 0;
-		border-bottom: 1px solid;
-
-		&:hover {
-			background-color: #333;
-		}
-	}
-	a {
-		@media (max-width: 768px) {
-			width: 100%;
-			background-color: black;
-			color: white;
-			border-radius: 0;
-			border-bottom: 1px solid white;
-
-			&:hover {
-				background-color: #333;
-			}
-		}
-	}
 `;
