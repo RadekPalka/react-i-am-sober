460247f79b88fa2234ad008d76d7c857932bd238
Radoslaw Palka <radekpalka@gmail..com>
2024-11-21 19:14:24 +0100

    style: update width in StyledForm

--- src/components/StyledForm.tsx
+++ src/components/StyledForm.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledForm = styled.form`
 	flex-direction: column;
 	justify-content: space-around;
-	width: 60%;
+	width: 75%;
 
 	margin: 0 auto;
 `;
