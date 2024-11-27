c7e7ba733dd71e2748292d031f6b81b31c2c055a
Radoslaw Palka <radekpalka@gmail..com>
2024-11-27 20:45:59 +0100

    refactor: remove unnecessary lines

--- src/components/StyledLabel.ts
+++ src/components/StyledLabel.ts
@@ -2,8 +2,5 @@ import styled from 'styled-components';
 
 export const StyledLabel = styled.label`
 	@media (max-width: 768px) {
-		width: 205px;
-		margin-bottom: 5px;
-		text-align: center;
 	}
 `;
