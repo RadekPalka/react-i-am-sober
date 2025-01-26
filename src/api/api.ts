d8175cdf5a0695fe0c2b5f313120c8d39a913ab3
Radosław Pałka <radekpalka@gmail.com>
2025-01-26 17:38:08 +0100

    eat: add types for import.meta.env and configure Vite environment

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
 	baseURL:
-		process.env.REACT_APP_API_URL || 'https://mentoring-api.vercel.app/api/v1',
+		import.meta.env.VITE_API_URL || 'https://mentoring-api.vercel.app/api/v1',
 });
 
 export default api;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly VITE_API_URL: string; // Zdefiniuj swoje zmienne środowiskowe
+	// Dodaj tutaj inne zmienne, jeśli są potrzebne
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
