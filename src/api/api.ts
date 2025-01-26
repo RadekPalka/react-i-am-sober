578f38c9dc817018787602a9a95ea3b22624d72a
Radosław Pałka <radekpalka@gmail.com>
2025-01-26 17:17:01 +0100

    feat: update baseURL for docker

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'https://mentoring-api.vercel.app/api/v1',
+	baseURL:
+		process.env.REACT_APP_API_URL || 'https://mentoring-api.vercel.app/api/v1',
 });
 
 export default api;
